ef4d3fe8a5219f501694109b8fc88ddeaddc2b5b
Pranav Desai <pranavdesai5678@gmail.com>
2022-11-06 20:26:45 +0530

    added comments

--- ProductService/src/main/java/com/pranavdesai/ProductService/ProductServiceApplication.java
+++ ProductService/src/main/java/com/pranavdesai/ProductService/ProductServiceApplication.java
@@ -7,6 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ProductServiceApplication {
 
 	public static void main(String[] args) {
+		//Starter Spring Config
+		//Bhfsdf
 		SpringApplication.run(ProductServiceApplication.class, args);
 	}
 
